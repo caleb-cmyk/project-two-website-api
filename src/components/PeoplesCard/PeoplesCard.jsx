23d90fca516746949b4a5ae8a588ca59318dcdb6
caleb-cmyk <caleb-cmyk>
2025-02-08 19:29:03 +0800

    formatNum fixed

--- src/components/PeoplesCard/PeoplesCard.jsx
+++ src/components/PeoplesCard/PeoplesCard.jsx
@@ -1,7 +1,6 @@
 import { useParams } from "react-router";
 import ethnicService from "../../services/ethnicService/ethnicService";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 const PeoplesCard = () => {
     const ethnicGroup = useParams().peopleId;
@@ -9,19 +8,22 @@ const PeoplesCard = () => {
 
     useEffect(() => {
         const fetchEthnicInfo = async () => {
-          const data = await ethnicService(ethnicGroup);
-          const cleanEthnicData = {
-            peopleName: data[0].PeopNameAcrossCountries,
-            category: data[0].AffinityBloc,
-            worldwidePopulation: data[0].PopulationPGAC,
-            populationInCountriesCount: data[0].CountOfCountries,
-          };
-          setEthnicInfo(cleanEthnicData);
+            const data = await ethnicService(ethnicGroup);
+            const formatNum = (num) => {
+                return num.toLocaleString();
+            };
+            const cleanEthnicData = {
+                peopleName: data[0].PeopNameAcrossCountries,
+                category: data[0].AffinityBloc,
+                worldwidePopulation: formatNum(data[0].PopulationPGAC),
+                populationInCountriesCount: data[0].CountOfCountries,
+            };
+            setEthnicInfo(cleanEthnicData);
         };
         fetchEthnicInfo();
-      }, [ethnicGroup]);
+    }, [ethnicGroup]);
 
-      return (
+    return (
         <>
             <h1>{ethnicInfo.peopleName}</h1>
             <ul>
